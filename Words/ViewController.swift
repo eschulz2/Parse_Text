557ea10506e73119450bb194acf5a54c67cbf420
Eric Schulz <eschulz2@gmail.com>
2015-12-14 16:08:11 -0600

    first parse added

---
+++ Words/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Words
+//
+//  Created by Eric Schulz on 12/14/15.
+//  Copyright © 2015 Eric Schulz. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+        }
+    }
+
+
+}
+
